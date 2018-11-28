b556f5815c7af278764a9aeb8fbfa212692d1e5b
wdabao <723157193@qq.com>
2018-11-28 14:19:00 +0800

    131

---
+++ ShangchengApplication.java
@@ -0,0 +1,12 @@
+package com.djrx.shangcheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShangchengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShangchengApplication.class, args);
+    }
+}
